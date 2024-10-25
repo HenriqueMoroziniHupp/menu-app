56c78e53348bbb2694717d228d95cb6a36775e4f
Henrique Hupp <henriquehupp@MBA.local>
2024-10-25 02:05:08 -0300

    add filter and snap scroll in categories

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,7 @@
 import type { IProduct } from '@/interfaces/IProduct'
 import productsAPI from '@/service/ProductsService'
 import { useUserStore } from '@/stores/userStore'
-import { FilterMatchMode } from '@primevue/core/api'
-import { onMounted, onUnmounted, ref, shallowRef } from 'vue'
+import { computed, onMounted, onUnmounted, ref, shallowRef } from 'vue'
 import { useHead } from '@unhead/vue'
 import ImageComponent from '@/components/ImageComponent.vue'
 
@@ -17,11 +16,32 @@ interface IProdByCat {
 const __categories = shallowRef<Element>()
 const isFixed = shallowRef(false)
 const userStore = useUserStore()
-const loading = ref(false)
-const productsByCategory = ref<IProdByCat[]>()
-const filters = ref({
-  global: { value: null, matchMode: FilterMatchMode.CONTAINS }
-})
+const loaded = ref(false)
+const productsByCategory = shallowRef<IProdByCat[]>([])
+const search = ref('')
+
+const filteredProductsByCategory = computed<IProdByCat[]>(() => {
+  return productsByCategory.value.reduce((acc: IProdByCat[], category) => {
+    // Filtra os produtos que correspondem à busca
+    const matchingProducts = category.products.filter(product =>
+      product.name?.toLowerCase().includes(search.value)
+    );
+
+    // Se a categoria tiver produtos correspondentes, a adiciona ao acumulador
+    if (matchingProducts.length > 0) {
+      return [
+        ...acc,
+        {
+          ...category,
+          products: matchingProducts // Inclui apenas produtos correspondentes
+        }
+      ];
+    }
+
+    return acc; // Se não houver correspondência, retorna o acumulador sem alterações
+  }, []);
+});
+
 
 onMounted(async () => {
   try {
@@ -32,6 +52,7 @@ onMounted(async () => {
 
     const response = await productsAPI.getCategoriesWithProducts(userStore.slug)
     productsByCategory.value = response.data as IProdByCat[]
+    loaded.value = true
   } catch (error) {
     console.error(error)
   }
@@ -43,28 +64,15 @@ onUnmounted(() => {
   window.removeEventListener('scroll', handlerListener)
 })
 
-function throttle<T>(fn: (this: T, ...args: any[]) => void, limit: number) {
-  let lastCall = 0
-  return function (this: T, ...args: any[]) {
-    const now = Date.now()
-    if (now - lastCall >= limit) {
-      lastCall = now
-      fn.apply(this, args)
-    }
-  }
-}
-
 const handlerParallax = () => {
   const parallaxImage = document.getElementById('parallax') as HTMLElement
   let scrollPosition = window.pageYOffset
-
-  // Movimenta a imagem mais devagar que o scroll
   parallaxImage.style.transform = `translateY(${scrollPosition * 0.6}px)`
 }
 
-const handlerFixedCategories = throttle(() => {
+const handlerFixedCategories = () => {
   isFixed.value = __categories.value?.getBoundingClientRect().top === 0
-}, 200)
+}
 
 const handlerListener = () => {
   handlerParallax()
@@ -88,11 +96,6 @@ const scrollToCategory = (categoryId: any) => {
 <template>
   <div id="parallax" class="parallax-container">
     <ImageComponent image-class="parallax-image" :image-url="userStore.userData.bannerUrl" />
-    <!-- <img
-      class="parallax-image"
-      src="https://media-cdn.tripadvisor.com/media/photo-s/1b/4d/61/c4/fachada.jpg"
-      alt="Imagem do Restaurante"
-    /> -->
     <div class="restaurant-name">
       <p class="text-3xl">{{ userStore.userData.name }}</p>
       <p class="text-sm">{{ userStore.userData.description }}</p>
@@ -100,31 +103,47 @@ const scrollToCategory = (categoryId: any) => {
   </div>
 
   <div class="content card menu-container rounded-t-3xl">
-    <p class="mb-2 text-2xl font-norma">Categorias</p>
-    <div
-      ref="__categories"
-      class="categories sticky top-0 overflow-x-auto whitespace-nowrap py-2 scrollbar-hide"
-      :class="{ categories__shadow: isFixed }"
-    >
-      <template v-for="category in productsByCategory" :key="category.id">
-        <Button
-          @click="scrollToCategory(category.id)"
-          :label="category.name"
-          severity="secondary"
-          class="mr-2"
-          rounded
-        />
-      </template>
-    </div>
-
-    <div class="products mb-8 mt-2" v-for="category in productsByCategory" :key="category.id">
-      <p :id="category.id.toString()" class="mb-4 text-2xl font-semibold">{{ category.name }}</p>
-      <div class="cards-container flex flex-row flex-wrap gap-4">
-        <template v-for="product in category.products" :key="product.id">
-          <CardProduct :product />
+    <p class="mb-2 text-2xl" :class="{'animate-pulse' : !loaded}">Categorias</p>
+    <template v-if="loaded">
+      <div
+        ref="__categories"
+        id="carousel"
+        class="categories sticky top-0 overflow-x-auto whitespace-nowrap py-2 snap-mandatory snap-x"
+        :class="{ categories__shadow: isFixed }"
+      >
+        <template v-for="category in filteredProductsByCategory" :key="category.id">
+          <Button
+            @click="scrollToCategory(category.id)"
+            :label="category.name"
+            severity="secondary"
+            class="mr-2 snap-center snap-normal"
+            rounded
+          />
         </template>
       </div>
-    </div>
+
+      <div class="products mb-8 mt-2" v-for="category in filteredProductsByCategory" :key="category.id">
+        <p :id="category.id.toString()" class="mb-4 text-2xl font-semibold">{{ category.name }}</p>
+        <div class="cards-container flex flex-row flex-wrap gap-4">
+          <template v-for="product in category.products" :key="product.id">
+            <CardProduct :product />
+          </template>
+        </div>
+      </div>
+      <p v-if="!filteredProductsByCategory.length">Não encontramos produtos</p>
+    </template>
+    <template v-else>
+      <Skeleton class="mt-4 mb-4" height="37px"></Skeleton>
+      <Skeleton width="6rem" height="28px" class="mb-4"></Skeleton>
+      <Skeleton class="mb-4" width="100%" height="250px"></Skeleton>
+      <Skeleton width="100%" height="250px"></Skeleton>
+    </template>
+  </div>
+  <div class="filter z-10 fixed bottom-0 p-2 w-full">
+    <IconField iconPosition="left">
+        <InputText type="text" v-model="search" placeholder="Pesquisar..."  class="w-full glass" />
+        <InputIcon class="pi pi-search" />
+    </IconField>
   </div>
   <ScrollTop
     :threshold="1000"
@@ -136,10 +155,9 @@ const scrollToCategory = (categoryId: any) => {
 <style lang="scss" scoped>
 .parallax-container {
   position: relative;
-  height: 40vh; /* Ocupa 40% da tela */
+  height: 40vh;
   overflow: hidden;
 
-  /* Imagem de fundo para o efeito paralaxe */
   .parallax-image {
     top: 0;
     left: 0;
@@ -147,10 +165,10 @@ const scrollToCategory = (categoryId: any) => {
     height: 100%;
     overflow: hidden;
     object-fit: cover;
-    transform: translateZ(0); /* Melhora o desempenho no efeito */
+    transform: translateZ(0);
     top: 0;
     left: 0;
-    will-change: transform; /* Otimiza a performance */
+    will-change: transform;
   }
 
   /* Nome do restaurante */
@@ -175,7 +193,6 @@ const scrollToCategory = (categoryId: any) => {
   background-image: linear-gradient(180deg, transparent 50%, rgba(0, 0, 0, 0.4));
 }
 
-/* Conteúdo abaixo da imagem */
 .content {
   background: var(--surface-ground);
   z-index: 1;
@@ -187,8 +204,6 @@ const scrollToCategory = (categoryId: any) => {
   .categories {
     z-index: 2;
     background: var(--surface-ground);
-    scroll-snap-type: x mandatory;
-    scroll-snap-align: center;
     margin: 0 -1.2rem;
 
     .p-button:nth-child(1) {
@@ -209,5 +224,15 @@ const scrollToCategory = (categoryId: any) => {
       0 4px 6px -1px rgb(0 0 0 / 0.1),
       0 2px 4px -2px rgb(0 0 0 / 0.1);
   }
+}
+
+.glass {
+  background: rgba(255, 255, 255, 0.6);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  outline: none;
 }
 </style>
