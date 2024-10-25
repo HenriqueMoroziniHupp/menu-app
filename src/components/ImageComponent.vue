1e0499dfdd133e0fe478aea9e0695f3fa7ebb750
Henrique Hupp <henriquehupp@MBA.local>
2024-10-25 02:03:17 -0300

    refact and reuse loadder class

--- src/components/ImageComponent.vue
+++ src/components/ImageComponent.vue
@@ -35,7 +35,7 @@ const onImageError = () =>
       @error="onImageError"
     />
     <Transition>
-      <div class="image-loading" v-if="loading"></div>
+      <div class="image-loading loader" v-if="loading"/>
     </Transition>
   </div>
 </template>
@@ -51,69 +51,6 @@ const onImageError = () =>
     height: 100%;
     width: 100%;
     object-fit: cover;
-  }
-}
-
-.image-loading {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgba(255, 255, 255, 0.6);
-  border-radius: inherit;
-}
-
-.image-loading::before {
-  content: '';
-  width: 50px;
-  aspect-ratio: 1;
-  border-radius: 50%;
-  border: 5px solid #514b82;
-  animation:
-    l20-1 0.8s infinite linear alternate,
-    l20-2 1.6s infinite linear;
-}
-
-@keyframes l20-1 {
-  0% {
-    clip-path: polygon(50% 50%, 0 0, 50% 0%, 50% 0%, 50% 0%, 50% 0%, 50% 0%);
-  }
-  12.5% {
-    clip-path: polygon(50% 50%, 0 0, 50% 0%, 100% 0%, 100% 0%, 100% 0%, 100% 0%);
-  }
-  25% {
-    clip-path: polygon(50% 50%, 0 0, 50% 0%, 100% 0%, 100% 100%, 100% 100%, 100% 100%);
-  }
-  50% {
-    clip-path: polygon(50% 50%, 0 0, 50% 0%, 100% 0%, 100% 100%, 50% 100%, 0% 100%);
-  }
-  62.5% {
-    clip-path: polygon(50% 50%, 100% 0, 100% 0%, 100% 0%, 100% 100%, 50% 100%, 0% 100%);
-  }
-  75% {
-    clip-path: polygon(50% 50%, 100% 100%, 100% 100%, 100% 100%, 100% 100%, 50% 100%, 0% 100%);
-  }
-  100% {
-    clip-path: polygon(50% 50%, 50% 100%, 50% 100%, 50% 100%, 50% 100%, 50% 100%, 0% 100%);
-  }
-}
-
-@keyframes l20-2 {
-  0% {
-    transform: scaleY(1) rotate(0deg);
-  }
-  49.99% {
-    transform: scaleY(1) rotate(135deg);
-  }
-  50% {
-    transform: scaleY(-1) rotate(0deg);
-  }
-  100% {
-    transform: scaleY(-1) rotate(-135deg);
   }
 }
 </style>
